fd0c591de98be9d0355cee0430688f799aa87c01
horaleon1 <horaleon1@gmail.com>
2019-08-14 15:33:05 -0500

    "create state on Nueva Cita working"

--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -2,8 +2,25 @@ import React, { Component } from "react";
 
 export default class NuevaCita extends Component {
   state = {
+     cita:{
+       mascota:'',
+       propietario:'',
+       fecha:'',
+       hora:'',
+       sintomas:''
 
-  };
+     }
+  }
+
+  handleChange = (e) =>{
+    this.setState({
+      cita:{
+        ...this.state.cita,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
   render() {
     return (
       <div className="card mt-5 py-5">
@@ -23,6 +40,8 @@ export default class NuevaCita extends Component {
                   className="form-control"
                   placeholder="Nombre Mascota"
                   name="mascota"
+                  onChange={this.handleChange}
+                  value={this.state.cita.mascota}
                 />
               </div>
             </div>
@@ -37,6 +56,8 @@ export default class NuevaCita extends Component {
                   className="form-control"
                   placeholder="Nombre Dueño Mascota"
                   name="propietario"
+                  onChange={this.handleChange}
+                  value={this.state.cita.propietario}
                 />
               </div>
             </div>
@@ -44,14 +65,18 @@ export default class NuevaCita extends Component {
             <div className="form-group row">
               <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
               <div className="col-sm-8 col-lg-4">
-                <input type="date" className="form-control" name="fecha" />
+                <input type="date" className="form-control" name="fecha" 
+                onChange={this.handleChange}
+                value={this.state.cita.fecha}/>
               </div>
             </div>
 
             <div className="form-group row">
               <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
               <div className="col-sm-8 col-lg-4">
-                <input type="time" className="form-control" name="Hora" />
+                <input type="time" className="form-control" name="hora" 
+                onChange={this.handleChange}
+                value={this.state.cita.hora}/>
               </div>
             </div>
 
@@ -64,6 +89,8 @@ export default class NuevaCita extends Component {
                   className="form-control"
                   name="sintomas"
                   placeholder="Describe los Sintomas"
+                  onChange={this.handleChange}
+                  value={this.state.cita.sintomas}
                 />
               </div>
             </div>
