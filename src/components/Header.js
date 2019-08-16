9c64a1660de61f2e9ff904b7671052482690d377
horaleon1 <horaleon1@gmail.com>
2019-08-16 18:05:25 -0500

    ready for build

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+// import PropTypes from 'prop-types'
+
 
 const Header = ({titulo}) => (
   <header>
     <h1 className="text-center">{ titulo } </h1>
   </header>
 );
+// Header.prototype = {
+//   titulo : PropTypes.func.isRequired
+// }
 
 export default Header;
--- src/components/ListaCita.js
+++ src/components/ListaCita.js
@@ -1,21 +1,28 @@
 import React from "react";
 import Cita from "./Cita";
 
-const ListasCitas = ({ citas, eliminarCita }) => (
-  <div className="card mt-2 py-5">
-    <div className="card-body">
-      <h2 className="card-title text-center">
-        {" "}
-        Administra las citas desde aquí
-      </h2>
+const ListasCitas = ({ citas, eliminarCita }) => {
 
-      <div className="lista-citas">
-        {citas.map(cita => (
-          <Cita key={cita.id} cita={cita}  eliminarCita={eliminarCita}/>
-        ))}
+//imprimir un mensaje en base a si hay citas o no
+
+const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administra las citas aqui'
+
+
+  return (
+    <div className="card mt-2 py-5">
+      <div className="card-body">
+        <h2 className="card-title text-center">
+          {mensaje}
+        </h2>
+
+        <div className="lista-citas">
+          {citas.map(cita => (
+            <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ListasCitas;
